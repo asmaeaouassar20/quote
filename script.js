970fab1ed4d57dea2edc54e8a802ca805eabbaee
asmaeaouassar20 <asmaeaouassar333@gmail.com>
2023-08-17 19:23:04 +0200

    styling the h1 tag, styling the button when hovering, preparing functions for users's reactions

---
+++ script.js
@@ -0,0 +1,10 @@
+function add_like(){}
+function add_dont_like(){}
+
+
+function coloringH1(){
+    document.getElementById('H1').style.color='palevioletred';
+}
+function removeCOLORH1(){
+    document.getElementById('H1').style.color='white';
+}
